8ab2ff651537a2afcb3d5c7216576e3b9e9b7c6f
Tim Ung <tim.a.ung.12@gmail.com>
2026-06-06 12:33:56 +1000

    trained modeland started working on improving visuals

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from ultralytics import YOLO
+# from ultralytics import YOLO
 
-model = YOLO('yolo26m')
+# model = YOLO('models/best.pt')
 
-results = model.predict('./data/raw/videos/dodgeball.mp4', save=True)
-print(results[0])
-print('===================================')
-for box in results[0].boxes:
-    print(box)
+# results = model.predict('./data/raw/videos/dodgeball.mp4', save=True)
+# print(results[0])
+# print('===================================')
+# for box in results[0].boxes:
+#     print(box)
+
+def main():
+  pass
+
+if __name__ == "__main__":
+  main()
